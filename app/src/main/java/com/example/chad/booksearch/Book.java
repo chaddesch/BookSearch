0f72ccbf5cbf367438f695070b2c7a29b8c00348
chaddesch <chaddesch4@gmail.com>
2016-12-31 23:55:03 -0500

    meh

--- app/src/main/java/com/example/chad/booksearch/Book.java
+++ app/src/main/java/com/example/chad/booksearch/Book.java
@@ -10,13 +10,17 @@ public class Book {
 
     private String mAuthor;
 
-    public Book(String title, String author){
+    public Book(String title, String author) {
         mTitle = title;
         mAuthor = author;
     }
 
-    public String getTitle() { return mTitle; }
+    public String getTitle() {
+        return mTitle;
+    }
 
-    public String getAuthor() { return mAuthor; }
+    public String getAuthor() {
+        return mAuthor;
+    }
 
 }
